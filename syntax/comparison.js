8b5e5a8e26b2feec79660a7b6729b25518871905
1eeseungjin <dltmdwls6766@hanyang.ac.kr>
2021-09-18 02:05:13 +0900

    2:05am

---
+++ syntax/comparison.js
@@ -0,0 +1,8 @@
+console.log(1==1);
+console.log(1==2);
+console.log(1>2);
+console.log(1<2);
+console.log(1===1); //참과 거짓 ==두개 ===정확하게 같은지
+console.log(1===2);
+var name = 1; //대입연산자 1 넣기
+console.log(name == 1); //비교 연산자 1과 같은지
